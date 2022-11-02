9d9f689ccd3182ef2db0d1adfbe8ee38c503dd2f
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2022-11-02 20:29:38 +0100

    portfolio page and script added

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    let left = document.querySelector(".scrollImages");
+    left.scrollBy(-350, 0)
+}
+
+function scrollr() {
+    let right = document.querySelector(".scrollImages");
+    right.scrollBy(350, 0)
+}
